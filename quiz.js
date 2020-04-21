d7da3fe31420c84137414d74bf7e11044201e673
ZeroRewind <47046541+ZeroRewind@users.noreply.github.com>
2020-04-20 20:04:01 -0400

    Update quiz.js
    required questions, no skpping

--- quiz.js
+++ quiz.js
@@ -4,11 +4,20 @@ var numberOfIncorrect = 0;
 var questionCounter = 0;
 
 var correct = ["q13", "q24", "q32", "q43", "q51", "q62", "q72", "q81", "q94", "q104"];
+
 var correctAnswers =[
  "By traveling along Snake way", "Senzu Bean", "SSJ2",
  "Reveals the power level of an opponent", "Chi Chi",
  "Frieza", "Namek", "Bubbles and Gregory",
  "To fight Goku", "Saiyan Saga"
+];
+
+var allIds =[
+"q11", "q12", "q13", "q14", "q21", "q22", "q23", "q24",
+"q31", "q32", "q33", "q34", "q41", "q42", "q43", "q44",
+"q51", "q52", "q53", "q54", "q61", "q62", "q63", "q64",
+"q71", "q72", "q73", "q74", "q81", "q82", "q83", "q84",
+"q91", "q92", "q93", "q94", "q101", "q102", "q103", "q104",
 ];
 
 // Initiation
@@ -20,15 +29,21 @@ $(".s0").on("click", function(){
 });
 
 // Handle questions answers
+
+
 for(let i=1; i<11; i++){
-    $(".s"+i).on("click", function(event){
+       
+    $(".f"+i).on("submit", function(event){
         event.preventDefault();
-    
+
         if ($("input[id="+correct[i-1]+"]:checked").length > 0) {
+            
+
             $(".correct").removeClass("none");
             $(".q"+i).addClass("none");
             
             $(".c1").on("click",function(){
+                
                 $(".correct").addClass("none");
 
                 $( ".q"+(i+1) ).removeClass("none");
@@ -43,9 +58,9 @@ for(let i=1; i<11; i++){
             $(".correct-incorrect").text(numberOfCorrect+" Correct/"+numberOfIncorrect+" Incorrect.");
             // console.log("yes");
         }
-
         
-        else{
+        else{        
+
             $(".q"+i).addClass("none");
 
             $(".incorrect").html("<h3>Your answer is incorrect.</h3>"+"<p>The correct answer was: "+ correctAnswers[i-1]+".</p>"+"<button type='submit' class='start c1'>CONTINUE</button>");
@@ -84,5 +99,7 @@ for(let i=1; i<11; i++){
             
             }
         });
+
+        
     });    
 }
